0e9e03d565e4ebb4cd16de3050a4d0b884252b25
batalini315 <315batalini@gmail.com>
2017-10-22 15:23:20 +0300

    adress img1

--- js/slide2.js
+++ js/slide2.js
@@ -37,14 +37,14 @@ function prevSlide2(){
 };
 	function nextSlide() {
 		
-		if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+		if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
 			translateWidth = -$('#viewport').width() * (slideCount + 1);
 			$('#slidewrapper').css({
 				'transform': 'translate(' - translateWidth + 'px, 0)',
 				'-webkit-transform': 'translate(' - translateWidth + 'px, 0)',
 				'-ms-transform': 'translate(' - translateWidth + 'px, 0)',
 			});
-			slideNow = 0;
+			slideNow = 1;
 		} else {
 			translateWidth = -$('#viewport').width() * (slideNow + 2);
 			$('#slidewrapper').css({
